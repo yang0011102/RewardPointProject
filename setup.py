f9ad869b6ab051456492e71ccbd856f2d159815a
yang0011102 <825483176@qq.com>
2020-08-03 17:20:11 +0800

    1 修复了管理积分不显示的问题

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ from Cython.Build import cythonize
 setup(
     name='Anything you want',
     ext_modules=cythonize([
-        "./InterfaceModules/activity.py", "./InterfaceModules/dd.py"
-        , "./InterfaceModules/order.py", "./InterfaceModules/shoppingCart.py", "./InterfaceModules/upload.py",
-        "Interface.py", "dispatcher_switch.py",
+        # "./InterfaceModules/activity.py", "./InterfaceModules/dd.py"
+        # , "./InterfaceModules/order.py", "./InterfaceModules/shoppingCart.py", "./InterfaceModules/upload.py",
+        "Interface.py",
+        # "dispatcher_switch.py",
     ], language_level=3
     ),
 )
