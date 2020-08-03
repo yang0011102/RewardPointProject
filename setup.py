3d321b79f713a438dc27820e456ac127c22ca3de
yang0011102 <825483176@qq.com>
2020-08-03 14:45:20 +0800

    1 保存setup

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from Cython.Build import cythonize
 setup(
     name='Anything you want',
     ext_modules=cythonize([
-        # "./InterfaceModules/activity.py", "./InterfaceModules/dd.py"
-        # , "./InterfaceModules/order.py", "./InterfaceModules/shoppingCart.py", "./InterfaceModules/upload.py",
+        "./InterfaceModules/activity.py", "./InterfaceModules/dd.py"
+        , "./InterfaceModules/order.py", "./InterfaceModules/shoppingCart.py", "./InterfaceModules/upload.py",
         "Interface.py", "dispatcher_switch.py",
     ], language_level=3
     ),
